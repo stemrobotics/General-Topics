28dfefdecaf92515d9fa56215209c99ec151396d
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-09-08 15:46:25 -0700

    Create If2.java

---
+++ If2.java
@@ -0,0 +1,20 @@
+if ( x == y )
+    z = 7;                // This statement is executed.
+else
+    z = 0;
+
+if ( x != y )
+    z = 7;
+else
+    z = 0;                // This statement is executed.
+
+if ( x == 0 )
+{
+    z = 99;               // This statement block is executed
+    k = 33;
+}
+else
+{
+     z++;
+     k += 2;
+}
