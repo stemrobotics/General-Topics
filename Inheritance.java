86190833a4c6c8c49f828f51d136f09c349c7947
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-09-08 14:36:54 -0700

    Create Inheritance.java

---
+++ Inheritance.java
@@ -0,0 +1,69 @@
+public class Animal
+{
+    public String name;
+    public int age;
+
+    public Animal()
+    {
+    }
+
+    public Animal(String name)
+    {
+        this.name = name;
+    }
+}
+public class Dog extends Animal
+{
+    public boolean licensed;
+
+    public Dog(String name)
+    {
+        super(name);                 // Invoke superclass constructor.
+    }
+
+    public void bark()
+    {
+        System.out.println(super.name + " says BARK!");
+    }
+}
+
+public class Cat extends Animal
+{
+    public boolean isPersian;
+
+    public Cat(String name, int age)
+    {
+        super(name);
+        super.age = age;             // Access superclass field.
+    }
+
+    public void meow()
+    {
+        System.out.println(super.name + " says MEOW!");
+    }
+}
+
+public class Animals
+{
+    public static void main(String []args)
+    {
+        Dog myDog = new Dog("rover");
+        myDog.age = 3;
+
+        Cat myCat = new Cat("kitty", 3);
+        myCat.age = 1;               // Change the age field from 3 to 1.
+
+        myDog.licensed = true;
+        myCat.isPersian = true;
+
+        myDog.bark();
+        myCat.meow();
+
+        System.out.println("My dog " + myDog.name + " is " + myDog.age + " years old");
+
+        Animal myAnimal = new Animal("roger");
+        myAnimal.age = 5;
+
+        myAnimal.bark();            // Fails since bark is not part of Animal.
+    }
+}
