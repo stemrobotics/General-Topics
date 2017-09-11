b36bd5f0715b7921bbb0ae73e364ca0b64cb19da
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-09-11 09:53:30 -0700

    Create While1.java

---
+++ While1.java
@@ -0,0 +1,8 @@
+while ( z < 10 ) z += 1;             // z is incremented repeatedly as long as z is less than 10.
+                                     // z will be = 10 after the while.
+while ( z < 10 )
+{
+    z += 1;                          // z and k are incremented repeatedly as long as z is less than 10.
+    k += 2;
+                                     // z = 10 and k = 20 after the while.
+}
