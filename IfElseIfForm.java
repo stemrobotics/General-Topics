efe5ae631adc8932cb007a08ece9467a92645ed5
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-09-11 13:08:52 -0700

    Create IfElseIfForm.java

---
+++ IfElseIfForm.java
@@ -0,0 +1,13 @@
+if ( boolean-value is true )
+    statement-1;
+else if ( boolean-value-1 )
+    statement-2;
+    
+You can also do:
+
+if ( boolean-value is true )
+    statement-1;
+else if ( boolean-value-1 is true )
+    statement-2;
+else
+    statement-3;
