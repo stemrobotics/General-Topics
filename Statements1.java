c88976f15e5642217eda77c559bf0ca7d4c4fa0e
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-09-11 12:58:38 -0700

    Create Statements1.java

---
+++ Statements1.java
@@ -0,0 +1,6 @@
+myVar1 = 3 + 5;    // myVar1 is assigned the value of 8.
+
+System.out.println("Hello World");    // A system method is called to perform an action, no result saved.
+
+myVar1 = myIntegerMethod();    // A method, which returns an integer is called and the returned integer is
+                               // stored in myVar1.
