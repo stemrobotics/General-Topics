d58b5f2e6ab2935d3006031a9bbfe39029b765fd
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-09-11 11:02:28 -0700

    Create Objects3.java

---
+++ Objects3.java
@@ -0,0 +1,10 @@
+Dog myDog = new Dog();
+myDog.breed = "collie";
+myDog.name = "Lassie";
+myDog.bark();
+Dog herDog = new Dog();
+herDog.breed = "sheperd";
+herDog.name = "Fido";
+herDog.bark();
+
+System.out.println("My dog is named " + myDog.name + " and her dog is named " + herDog.name);
