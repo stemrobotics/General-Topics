5443986fdece700bbf724edb6b189333007f31a6
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-09-11 09:10:06 -0700

    Create Switch2.java

---
+++ Switch2.java
@@ -0,0 +1,16 @@
+switch ( day )
+{
+    case "monday":
+        dayNumber = 1;
+        break;
+
+    case "tuesday":
+         dayNumber = 2;
+         break;
+
+    case "wednesday":
+         dayNumber = 3;
+         break;
+
+    and so on for the rest of the week...
+}
