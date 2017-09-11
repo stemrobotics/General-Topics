4306e0adc7ec15eec6e75e7022b3382329ff2911
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-09-11 10:21:13 -0700

    Create SwitchForm.java

---
+++ SwitchForm.java
@@ -0,0 +1,13 @@
+switch ( switch-value )
+{
+    case selection-value-1:
+        statement-1;
+
+    case selection-value-2:
+        statement-2;
+        statement-3;
+        break;
+
+    default:                     // default is optional.
+        statement-4;
+}
