dc0939bfb8930035a43aeff620d549d6b465df7e
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-09-11 11:41:02 -0700

    Create Static1.java

---
+++ Static1.java
@@ -0,0 +1,19 @@
+class MyClass
+{
+    public int theVar;
+
+    public String toString()
+    {
+        return "theVar=" + theVar;
+    }
+}
+
+{ some other code
+    MyClass instance1 = new MyClass();
+    MyClass instance2 = new MyClass();
+
+    instance1.theVar = 3;
+    instance2.theVar = 7;
+
+    System.out.println(instance1.toString() + "   " + instance2.toString());
+}
