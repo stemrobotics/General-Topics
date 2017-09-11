8d7d6f3493ad85371298249e2ac1521e120219f9
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-09-11 11:41:31 -0700

    Create Static2.java

---
+++ Static2.java
@@ -0,0 +1,33 @@
+class MyClass2
+{
+    public static int globalCount = 0;
+    public int theVar = 0;
+
+    // class constructor counts each new instance of the class.
+
+    public MyClass2()
+    {
+        globalCount++;
+    }
+
+    public static int getTheCount()
+    {
+        return globalCount;
+    }
+}
+
+{ some other code
+    System.out.println("instance count=" + MyClass2.getTheCount());
+
+    MyClass2 instance1 = new MyClass2();
+    MyClass2 instance2 = new MyClass2();
+   
+    // access the static (global) variable directly using the
+    // name of the class not an instance reference.
+
+    MyClass2.globalCount++;
+
+    System.out.println("instance count=" + MyClass2.getTheCount());
+   
+    MyClass2.theVar++; 
+}
