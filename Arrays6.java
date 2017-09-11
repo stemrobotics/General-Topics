70fac5228b962f89aafa81f8b9fdfc411244b765
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-09-11 12:31:38 -0700

    Rename Arrays5.java to Arrays6.java

---
+++ Arrays6.java
@@ -0,0 +1,9 @@
+java.util.ArrayList <String>al = new java.util.ArrayList<String>();
+
+al.add("A String object");                  // Add an element to the ArrayList.
+al.add("A second String object");
+al.set(1, "A different String object");     // Change the element at index zero.
+
+al.remove(0);                               // Remove the element at index zero.
+
+String s = al.get(0);                       // Get the element at index zero.
