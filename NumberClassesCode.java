0ae22c7d737980c4787edb5676f351818803cb41
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-09-12 08:14:46 -0700

    Create NumberClassesCode.java

---
+++ NumberClassesCode.java
@@ -0,0 +1,15 @@
+public class HelloWorld
+{
+     public static void main(String []args)
+     {
+        Integer i = 3;
+        
+        i = i + 2;
+        
+        System.out.println("i=" + i + "  i=" + i.toString());
+        
+        int j = Integer.parseInt("123");
+                
+        System.out.println("j=" + j + "  j=" + j.toString());    
+     }
+}
