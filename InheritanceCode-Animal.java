4e86a7576bb08877d580fcae452e40ab3b6932e3
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-09-12 08:05:04 -0700

    Create InheritanceCode-Animal.java

---
+++ InheritanceCode-Animal.java
@@ -0,0 +1,14 @@
+public class Animal
+{
+    public String name;
+    public int age;
+
+    public Animal()
+    {
+    }
+
+    public Animal(String name)
+    {
+        this.name = name;
+    }
+}
