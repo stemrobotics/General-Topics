afbb8d1c78a395c318d36b9cbf40723da077086c
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-09-12 07:48:44 -0700

    Create ReferenceCode.java

---
+++ ReferenceCode.java
@@ -0,0 +1,54 @@
+public class HelloWorld
+{
+     public static void main(String []args)
+     {
+        String myStr = "abc", myStr3;
+
+        myMethod(myStr);
+        
+        System.out.println("myStr=" + myStr);
+        
+        StringBuffer mySB = new StringBuffer("abc");
+      
+        myMethod1(mySB);
+                
+        System.out.println("mySB=" + mySB);
+      
+        myMethod2(mySB);
+                
+        System.out.println("mySB=" + mySB);
+        
+        myStr = "abc";
+        
+        myStr3 = myMethod3(myStr);
+        
+        System.out.println("\nmyStr=" + myStr);
+        System.out.println("myStr3=" + myStr3);
+     }
+
+    public static void myMethod( String str )
+    {
+        str = str + "xyz";
+        System.out.println("\n  str=" + str);
+    }
+
+    public static void myMethod1( StringBuffer sb )
+    {
+        sb.append( "xyz" );
+        System.out.println("\n  sb=" + sb);
+
+    }
+    
+    public static void myMethod2( StringBuffer sb )
+    {
+        sb = new StringBuffer( "123");
+        System.out.println("\n  sb=" + sb);
+
+    }
+    
+    public static String myMethod3( String str )
+    {
+         str = str + "xyz";
+         return str;
+    }
+}
