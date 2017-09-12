52602923e9f15bf298693a842dd6c1333633bc5b
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-09-12 08:08:45 -0700

    Create OverridingCode-Animal.java

---
+++ OverridingCode-Animal.java
@@ -0,0 +1,14 @@
+public class Animal
+{
+    public String name;
+
+    public Animal(String name)
+    {
+        this.name = name;
+    }
+    
+    public String whatIsMyName()
+    {
+        return("I am an Animal named " + name);
+    }
+}
