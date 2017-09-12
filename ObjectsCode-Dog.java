86fc43574bf06e6652abb47553e4dd2fd1bf73d4
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-09-12 08:02:53 -0700

    Create ObjectsCode-Dog.java

---
+++ ObjectsCode-Dog.java
@@ -0,0 +1,10 @@
+public class Dog
+{
+      public String breed, name;  // each dog instance will have two string variables: breed and name.
+      public int    age;          // each dog instance also has an integer variable: age.
+
+      public void bark()
+      {
+            System.out.println("The " + breed + " " + name + " is barking!");
+      }
+}
