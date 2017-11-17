827d17ce5fc2ffc01caa191e4361a84f806687c1
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2017-11-17 14:00:15 -0800

    Create PIDImplementation.java

---
+++ PIDImplementation.java
@@ -0,0 +1,11 @@
+public class MyEncoder implements PIDSource
+{
+    // fields, constructors and other methods.
+    
+    public double pidGet()
+    {
+        pidValue = some computation or process resulting in the value we want to return.
+        
+        return pidValue;
+    }
+}
