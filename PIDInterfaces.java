5bf02248095e2bb6fa1c280e7704bbd0d92f69b6
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2018-04-17 14:34:37 -0700

    Update PIDInterfaces.java

--- PIDInterfaces.java
+++ PIDInterfaces.java
@@ -1,18 +1,18 @@
 /**
- * This interface allows for PIDController to automatically read from this object.
+ * This interface allows PIDController to read from any implementing object.
  */
 public interface PIDSource 
 {
   /**
-   * Get the result to use in PIDController.
+   * Get the source value to use in PIDController.
    *
-   * @return the result to use in PIDController
+   * @return the source value to use in PIDController
    */
   double pidGet();
 }
 
 /**
- * This interface allows PIDController to write it's results to its output.
+ * This interface allows PIDController to write it's results to any implementing object.
  */
 public interface PIDOutput 
 {
