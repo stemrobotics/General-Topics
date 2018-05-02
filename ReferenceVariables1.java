ea7046f468ee7986de838f0edd8982cf88280451
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2018-05-02 13:04:16 -0700

    Update ReferenceVariables1.java

--- ReferenceVariables1.java
+++ ReferenceVariables1.java
@@ -1,7 +1,7 @@
-String myString = new String("hello");    // Sets myString to a new String instance that contains "hello".
+String myString = new String("hello");    // Sets myString to a new String object instance that contains "hello".
 
 String myString = "hello";                // Special case of String that does the same as the new keyword.
 
-String myString1 = myString;              // myString1 now references myString.
-                                          // Both strings point to the same instance of a
-                                          // String containing "hello".
+String myString1 = myString;              // myString1 now references myString, that is both
+                                          // String variables point to the same instance of a
+                                          // String object containing "hello".
