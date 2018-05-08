0974ccdabac61607b5d138cec27cb8458da73adf
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2018-05-08 15:55:33 -0700

    Update While1.java

--- While1.java
+++ While1.java
@@ -4,5 +4,4 @@ while ( z < 10 )
 {
     z += 1;                          // z and k are incremented repeatedly as long as z is less than 10.
     k += 2;
-                                     // z = 10 and k = 20 after the while.
-}
+}                                     // z = 10 and k = 20 after the while.
