311dd52f7a147b0431a6699830130af410622ca2
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2018-05-08 15:28:54 -0700

    Update Switch1.java

--- Switch1.java
+++ Switch1.java
@@ -1,3 +1,5 @@
+int x = 2;
+
 switch ( x )
 {
     case 1:
@@ -5,11 +7,11 @@ switch ( x )
 
     case 2:
         z = 5;
-        break;
+        break;      // Ends case 2.
 
     case 3:
          z = 9;
 
     default:
-         z = 11;
+         z = 11;    // No case(s) matched.
 }
