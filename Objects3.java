1ff78e0677a88fab9231476ea0a557e3ecaaa1c0
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2018-05-10 13:55:28 -0700

    Update Objects3.java

--- Objects3.java
+++ Objects3.java
@@ -2,9 +2,16 @@ Dog myDog = new Dog();
 myDog.breed = "collie";
 myDog.name = "Lassie";
 myDog.bark();
+
 Dog herDog = new Dog();
-herDog.breed = "sheperd";
+herDog.breed = "shepherd";
 herDog.name = "Fido";
 herDog.bark();
 
-System.out.println("My dog is named " + myDog.name + " and her dog is named " + herDog.name);
+System.out.println("My dog is named " + myDog.name + " and her dog is named " + herDog.name + ".");
+
+This prints out:
+
+The collie Lassie is barking!
+The shepherd Fido is barking!
+My dog is named Lassie and her dog is named Fido.
