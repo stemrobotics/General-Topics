cbfcbdd799e48b68787929931b9a349e3110109d
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2018-05-10 13:52:11 -0700

    Update Objects2.java

--- Objects2.java
+++ Objects2.java
@@ -1,4 +1,5 @@
-Dog myDog = new Dog();            // myDog isn't a primitive, its an instance of class Dog.
+Dog myDog = new Dog();            // myDog isn't a primitive, its a referemce
+                                  // to an instance of class Dog.
 myDog.breed = "collie";
 myDog.name = "Lassie";
 myDog.age = 5;
@@ -7,3 +8,9 @@ myDog.bark();
 
 System.out.println("My dog is a " + myDog.breed + " named " + myDog.name +
                    " and is " + myDog.age + " years old.");
+
+This prints out:
+
+The collie Lassie is barking!
+My dog is a collie named Lassie and is 5 years old.
+  
