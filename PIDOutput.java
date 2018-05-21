915ca1b1c71978b2b7bfce2ec0d4c4f2e46b416d
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2018-05-21 12:41:06 -0700

    Create PIDOutput.java

---
+++ PIDOutput.java
@@ -0,0 +1,9 @@
+public class MyOutputClass implements PIDOutput
+{
+    // fields, constructors and other methods.
+    
+    public void pidWrite(double value)
+    {
+        Perform some computation or process using the value passed in;
+    }
+}
