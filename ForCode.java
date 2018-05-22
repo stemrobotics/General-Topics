48e572e21ff318e362b4ed385558c49483865f9f
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2018-05-22 09:27:47 -0700

    Update ForCode.java

--- ForCode.java
+++ ForCode.java
@@ -4,21 +4,25 @@ public class HelloWorld
     {
         System.out.println("FOR loop examples");
             
-        for ( int i = 1; i < 4; i++ ) System.out.println( "Count is: " + i );
+        for ( int i = 1; i < 4; i++ ) System.out.println( "Count 1 is: " + i );
     
         // This statement will cause a compile error because i is local to the
         // for statement and not available outside of it. Comment out the next
         // line to compile and run the example.
         System.out.println("Final i is:" + i);
         
+        System.out.println();
+        
         int j;
     
         for ( j = 1 ; ; )
         {
-            System.out.println( "Count is: " + j );
+            System.out.println( "Count 2 is: " + j );
             if ( j == 3) break;
             j++;
         }
+        
+        System.out.println();
 
         // We can use j because it is defined in the same scope as the println.
         System.out.println("Final j is:" + j);
