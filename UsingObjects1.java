805850ca3f21ebf1a226e49b64d21e4e0b5130eb
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2018-06-13 10:50:59 -0700

    Update UsingObjects1.java

--- UsingObjects1.java
+++ UsingObjects1.java
@@ -1,5 +1,7 @@
 public class Class1
 {
+      // This method wants a reference to an instances of Class2 passed
+      // to it when called.
       public void class1Method( Class2 class2Instance )
       {
             System.out.println( class2Instance.name );
@@ -10,10 +12,25 @@ public class Class2
 {
       public String name = "Class2";
 
-      public void someMethod()
+      // The method parameter "name" hides the class level variable
+      // above inside the method.
+      
+      public void someMethod(String name)
       {
+            // name by itself refers to the parameter variable.
+            // this.name refers to the class level variable.
+            // The effect if this statement is to save the value
+            // passed into the method in the class level variable so
+            // that information persists after the method ends.
+            
+            this.name = name;
+            
             Class1 c1 = new Class1();
 
+            // Here we are calling the method on Class1 that wants a reference
+            // to an instance of Class2, to which we will pass this instance of
+            // Class2 by using the this keyword.
+            
             c1.class1Method( this );
       }
 }
