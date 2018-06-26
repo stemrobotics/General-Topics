010881f85f9fa0ae413cd96de69c2da4749e0539
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2018-06-26 15:00:25 -0700

    Update Overloading2.java

--- Overloading2.java
+++ Overloading2.java
@@ -1,13 +1,22 @@
-public double computeArea( int heightInFeet, int widthInFeet )
+public double computeArea( double heightInFeet, double widthInFeet )
 {
-      return heightInFeet * widthInFeet;
+    return heightInFeet * widthInFeet;
 }
 
-public double computeArea( int heightInFeet, int widthInFeet, boolean returnMeters )
+public double computeArea( double heightInFeet, double widthInFeet, boolean returnMeters )
 {
-      double area = computeArea( heightInFeet, widthInFeet );
+    double area = computeArea( heightInFeet, widthInFeet );
 
-      if ( returnMeters ) area = area * .3048;
+    if ( returnMeters ) area = area * .3048;
 
-      return area;
+    return area;
+}
+
+public static void main(String []args)
+{
+    double  h = 10, w=10;
+    
+    System.out.println("area=" + computeArea(10, 10));
+    System.out.println("area=" + computeArea(h, 10, false));
+    System.out.println("area(m)=" + computeArea(h, w, true));
 }
