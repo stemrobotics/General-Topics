df318495153c7975afc4732c39390ad62e5042b8
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2018-09-20 13:27:53 -0700

    Create Switch3.java

---
+++ Switch3.java
@@ -0,0 +1,13 @@
+switch (foo) 
+{
+    case 0:
+    case 1:
+        doSomething();
+        break;
+    case 2:
+        doSomethingElse();
+        break;
+    default:
+        doSomeOtherThing();
+        break;
+}
