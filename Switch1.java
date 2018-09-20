1159773e1a927a40396be1493c3176ab66c7cb2a
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2018-09-20 13:15:12 -0700

    Update Switch1.java

--- Switch1.java
+++ Switch1.java
@@ -3,7 +3,9 @@ int x = 2, z;
 switch ( x )
 {
     case 1:
-        z = 1;
+        z = 1;      // this statement is executed
+                    // then continue into case 2 as
+                    // there is no break for case 1.
 
     case 2:
         z = 5;
