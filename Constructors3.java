fe051301b26721dfe7e59a6f48d58393d85ac90f
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2019-08-22 17:49:06 -0700

    Update Constructors3.java

--- Constructors3.java
+++ Constructors3.java
@@ -15,3 +15,15 @@ This prints out:
 My  is barking!
 My collie  is barking!
 My collie Lassie is barking!
+  
+// Note, we can also do this:
+
+Dog myDog = new Dog();
+
+myDog.name = "Lassie";
+myDog.breed = "Collie";
+
+// This works because breed and name have public
+// access so we can use the dot to change them.
+// If they were private variables, we would have
+// to use a constructor to initialize them.
