2266409a377f857128248ed34c60a68ba379990e
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2019-08-22 17:44:21 -0700

    Update Constructors2.java

--- Constructors2.java
+++ Constructors2.java
@@ -2,15 +2,25 @@ public class Dog
 {
       public String breed, name = "";
 
+      // name is initialized explicitly above to an empty string.
+      // Below, a constructor method initializes breed the same way.
+      // These two ways to initialize variables are equivalent.
+      
       public Dog()
       {
             breed = "";
       }
 
+      // This constructor allows us to set the breed to whatever we
+      // want when we create a Dog instance with the new keyword.
+      
       public Dog(String breed)
       {
             this.breed = breed;
       }
+      
+      // This constructor allows us to set both breed and name on
+      // the new keyword.
       
       public Dog(String breed, String dogName)
       {
