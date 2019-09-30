014a172234028561c3fadbf65081a48d92affe2a
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2019-09-30 12:05:59 -0700

    Update Statements2.java

--- Statements2.java
+++ Statements2.java
@@ -1,2 +1,5 @@
-myVar1 = 5 + (2 * 2) * 3;     // 2 is squared, then multiplied by 3 and 5 is added to that result so 
+myVar1 = 5 + 2 + 2 * 3;       // 2 is multiplied by 3, then 5 is added to 2 which is added to 6 so
+                              // myVar1 = 13.
+
+myVar1 = 5 + (2 + 2) * 3;     // 2 is added to 2, the result multipled by 3, the result added to 5 so 
                               // myVar1 = 17.
