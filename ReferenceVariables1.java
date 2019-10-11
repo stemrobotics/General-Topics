b332606d0ad5063f860aee67a3e72ed3e20876be
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2019-10-11 13:19:42 -0700

    Update ReferenceVariables1.java

--- ReferenceVariables1.java
+++ ReferenceVariables1.java
@@ -3,5 +3,8 @@ String myString = new String("hello");    // Sets myString to point to a new Str
 
 String myString = "hello";                // Special case of String that does the same as the new keyword.
 
+String myString1 = "goodbye";             // Now we wave two instances of the object String, one containing
+                                          // "hello" and one containing "goodbye".
+
 String myString1 = myString;              // myString1 now points to the same instance of a String object
                                           // containing "hello" that myString points to.
