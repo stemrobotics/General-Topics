2968ea3552a2fe7fd7d3cd27301faf724698216b
StemRobotics <31777182+stemrobotics@users.noreply.github.com>
2019-10-11 13:23:49 -0700

    Update ReferenceVariables1.java

--- ReferenceVariables1.java
+++ ReferenceVariables1.java
@@ -1,10 +1,12 @@
 String myString = new String("hello");    // Sets myString to point to a new String object instance that
                                           // contains "hello".
 
-String myString = "hello";                // Special case of String that does the same as the new keyword.
-
-String myString1 = "goodbye";             // Now we wave two instances of the object String, one containing
+String myString1 = "goodbye";             // Special case of String that does the same as the new keyword.
+                                          // This only works for Strings.
+                                          // Now we have two instances of the object String, one containing
                                           // "hello" and one containing "goodbye".
 
 String myString1 = myString;              // myString1 now points to the same instance of a String object
-                                          // containing "hello" that myString points to.
+                                          // containing "hello" that myString points to. The "goodbye" 
+                                          // instance is no longer pointed to by a reference variable so it
+                                          // is deleted from memory.
